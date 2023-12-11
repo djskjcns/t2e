94d60738d93646ff1ecdcafbea22509e94fbfb0b
djskjcns <79205062+djskjcns@users.noreply.github.com>
2023-12-11 23:43:01 +0800

    Update main.py .gitignore

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import os
+import re
 import ebooklib
 from ebooklib import epub
 from lxml import etree
 
-def read_file(file_path):
-    with open(file_path, 'r') as file:
-        text = file.read()
+def import_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = [line.lstrip() for line in file.readlines()]
+        text = ''.join(lines)
+
+    chapter_regex = r'(.{0,10}第.*章.{0,30})'
+    
+    first_line = lines[0] if lines else ""
+    if re.match(chapter_regex, first_line):
+        text = re.sub(f'(?m){chapter_regex}', r'\n\1', text)
+        text = re.sub(r'\n+(.{0,10}第.*章.{0,30})', r'\1', text, count=1)
+    else:
+        text = re.sub(f'(?m){chapter_regex}', r'\n\1', text)
+
     return text
 
 def create_book(identifier, title, language, author, cover_image):
@@ -17,7 +29,7 @@ def create_book(identifier, title, language, author, cover_image):
     book.set_cover(cover_image, open(cover_image, 'rb').read())
     return book
 
-def add_stylesheet(book):
+def process_chapters(book, text):
     style = '''
     @namespace epub "http://www.idpf.org/2007/ops";
     body {
@@ -31,25 +43,19 @@ def add_stylesheet(book):
     '''
     default_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
     book.add_item(default_css)
-    return default_css
-
-def create_chapter(book, default_css, chapter_text, index):
-    lines = chapter_text.split('\n')
-    title = lines[0] if len(lines) > 0 else 'Chapter {}'.format(index+1)
-    content = '</p><p>'.join(lines[1:]) if len(lines) > 1 else ''
-    c = epub.EpubHtml(title=title, file_name='chap_{}.xhtml'.format(index+1), lang=book.language)
-    c.content=u'<h2>{}</h2><p>{}</p>'.format(title, content)
-    c.add_item(default_css)
-    book.add_item(c)
-    return c
-
-def add_chapters_to_book(book, default_css, text):
+    
     chapters = text.split("\n\n")
     book_chapters = []
     book_spine = ['nav']
 
-    for i, chapter in enumerate(chapters):
-        c = create_chapter(book, default_css, chapter, i)
+    for i, chapter_text in enumerate(chapters):
+        lines = chapter_text.split('\n')
+        title = lines[0] if lines else 'Chapter {}'.format(i + 1)
+        content = '</p><p>'.join(lines[1:]) if len(lines) > 1 else ''
+        c = epub.EpubHtml(title=title, file_name='chap_{}.xhtml'.format(i + 1), lang=book.language)
+        c.content = u'<h2>{}</h2><p>{}</p>'.format(title, content)
+        c.add_item(default_css)
+        book.add_item(c)
         book_chapters.append(c)
         book_spine.append(c)
 
@@ -58,23 +64,24 @@ def add_chapters_to_book(book, default_css, text):
     book.add_item(epub.EpubNav())
     book.spine = book_spine
 
-def output_book(book, output_path):
+    return book
+
+def export_book(book, output_path):
     epub.write_epub(output_path, book, {})
 
 def main():
-    file_path = '地煞七十二变.txt'
+    file_path = '我的病娇仙子模拟器.txt'
     identifier = 'id123456'
-    title = '地煞七十二变'
+    title = '我的病娇仙子模拟器'
     language = 'zh-CN'
-    author = '祭酒'
-    cover_image = 'ds.jpeg'
-    output_path = '地煞七十二变.epub'
+    author = '憨八龟爱上大大怪'
+    cover_image = 'wd.png'
+    output_path = '我的病娇仙子模拟器.epub'
     
-    text = read_file(file_path)
+    text = import_file(file_path)
     book = create_book(identifier, title, language, author, cover_image)
-    default_css = add_stylesheet(book)
-    add_chapters_to_book(book, default_css, text)
-    output_book(book, output_path)
+    book = process_chapters(book, text)
+    export_book(book, output_path)
 
 if __name__ == "__main__":
     main()
